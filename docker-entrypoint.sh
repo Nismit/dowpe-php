cbeb5c9b4698c5ab744b7844589c00301ac4f7f6
Nismit <michinobu@drivedigital.ca>
2018-05-16 16:56:32 -0700

    Add example code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,6 +11,29 @@ WP_VERSION=${WP_VERSION:-latest}
 WP_LOCALE=${WP_LOCALE:-en_US}
 WP_THEME=${WP_THEME:-twentyseventeen}
 
+# Color Collection
+NORMAL=$(tput sgr0)
+CYAN=$(tput setaf 6)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2; tput bold)
+RED=$(tput setaf 1)
+
+function error() {
+  echo -e "$RED$*$NORMAL"
+}
+
+function success() {
+  echo -e "$GREEN$*$NORMAL"
+}
+
+function warn() {
+  echo -e "$YELLOW$*$NORMAL"
+}
+
+function info() {
+  echo -e "$CYAN$*$NORMAL"
+}
+
 # wp --allow-root core download
 
 # TODO
@@ -19,9 +42,24 @@ WP_THEME=${WP_THEME:-twentyseventeen}
 # install theme and remove others
 # install plugins
 
+
+
 main() {
+
+  #until nc -z -v -w30 $DB_HOST 3306
+  #do
+    #echo "Waiting for database connection..."
+    # wait for 5 seconds before check again
+    #sleep 5
+  #done
+
   echo 'test'
   echo $DB_HOST
+
+  #if [ -z "" ]; then
+  #  error 'Error! Can not complete to install WordPress'
+  #  exit 1
+  #fi
 }
 
 main
