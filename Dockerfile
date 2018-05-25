92f00a27c4b3db791e5413f8acdc60bd89438897
Michinobu Nishimoto <nismit.dev@gmail.com>
2018-05-24 22:59:19 -0700

    :tada: Add shell script to install WordPress and config (#4)
    * :sparkles: Update README and add dockerignore to LICENSE file
    
    * :rocket: WIP add shell script
    
    * :rocket: Add example code
    
    * :sparkles: Update shellscript to set up WordPress
    
    * :sparkles: Add mysql-client for wp-cli and remove wp-cli version after it installed
    
    * :sparkles: Modify wordpress installation shell script
    
    * :rocket: Update shell script to add clean up and install plugins
    
    * :sparkles: Update README.md to add mysql-client

--- Dockerfile
+++ Dockerfile
@@ -29,6 +29,7 @@ RUN set -ex \
               imagemagick-dev \
         # Package Install
         && apk add --no-cache --update \
+              mysql-client \
               libmemcached \
               freetype \
               libjpeg-turbo \
@@ -49,3 +50,15 @@ RUN set -ex \
         && apk del .phpize-deps .build-deps \
         # Clean Up
         && rm -rf /tmp/*
+
+# install composer and wp-cli
+RUN set -ex \
+        && curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer \
+        && curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+        && chmod +x wp-cli.phar \
+        && mv wp-cli.phar /usr/local/bin/wp
+
+# Copy and prepare entrypoint
+COPY docker-entrypoint.sh /
+RUN chmod +x /docker-entrypoint.sh
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+# Environment Variable
+MYSQL_HOST=${MYSQL_HOST:-mysql}
+MYSQL_DATABASE=${MYSQL_DATABASE:-wordpress}
+MYSQL_USER=${MYSQL_USER:-dowpe_user}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-dowpe_password}
+MYSQL_PREFIX=""
+
+# WordPress Variable
+WP_VERSION=${WP_VERSION:-latest}
+WP_LOCALE=${WP_LOCALE:-en_US}
+WP_THEME=${WP_THEME:-twentyseventeen}
+WP_PLUGINS=${WP_PLUGINS:-wp-multibyte-patch\ duplicate-post}
+WP_URL=${WP_URL:-localhost\:8080}
+WP_TITLE=${WP_TITLE:-Dowpe-WordPress}
+WP_ADMIN=${WP_ADMIN:admin}
+WP_PASSWORD=${WP_PASSWORD:-password}
+WP_EMAIL=${WP_EMAIL:-example\@example.com}
+
+# Color Collections
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+BOLD='\033[1m'
+NC='\033[0m'
+
+success() {
+  echo -e ${GREEN}${BOLD}$1${NC}
+}
+
+error() {
+  echo -e ${RED}$1${NC}
+}
+
+info() {
+  echo -e ${CYAN}$1${NC}
+}
+
+wp_download() {
+  wp --allow-root core download --version=$WP_VERSION --locale=$WP_LOCALE
+}
+
+wp_create_config() {
+  wp --allow-root config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --dbprefix=${MYSQL_PREFIX}
+}
+
+wp_install_plugins() {
+  wp --allow-root plugin install ${WP_PLUGINS} --activate
+}
+
+wp_clean_up_plugins() {
+  wp --allow-root plugin delete $(wp --allow-root plugin list --status=inactive --field=name)
+}
+
+wp_clean_up_themes() {
+  wp --allow-root theme delete $(wp --allow-root theme list --status=inactive --field=name)
+}
+
+wp_install() {
+    wp --allow-root core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_PASSWORD} --admin_email=${WP_EMAIL}
+}
+
+main() {
+  if ! $(wp --allow-root core is-installed); then
+    info 'Start WordPress Installation'
+    wp_download
+
+    until nc -z -v -w10 $MYSQL_HOST 3306
+    do
+      info "Waiting for database connection..."
+      sleep 5
+    done
+
+    success 'Database established!'
+
+    # Check exists wp-config.php
+    wp --allow-root config path --quiet
+    hasConfig=$?
+
+    if [ $hasConfig -ne 0 ]; then
+      info 'Not found wp-config.php'
+      info 'Generating wp-config.php'
+      wp_create_config
+    fi
+
+    info 'wp-config.php path: '
+    wp --allow-root config path
+
+    # Install WordPress
+    wp_install
+
+    # Clean up plugins which are inactive
+    wp_clean_up_plugins
+
+    # Install listed plugins
+    wp_install_plugins
+
+    # Clean up themes which are inactive
+    # Because of security risk that having old theme
+    wp_clean_up_themes
+  fi
+}
+
+main
