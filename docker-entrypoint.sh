755b90874c49b0e6543e019909fe7d4c91305d46
git <nismit.dev@gmail.com>
2018-05-26 17:12:11 -0700

    :sparkles: Modify WordPress install shell script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -42,50 +42,57 @@ wp_download() {
 }
 
 wp_create_config() {
-  wp --allow-root config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --dbprefix=${MYSQL_PREFIX}
-}
+  wp --allow-root config path --quiet
 
-wp_install_plugins() {
-  wp --allow-root plugin install ${WP_PLUGINS} --activate
-}
+  if [ $? -eq 1 ]; then
+    info 'Not found wp-config.php'
+    info 'Generating wp-config.php'
+    wp --allow-root config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --dbprefix=${MYSQL_PREFIX} --color
+  fi
 
-wp_clean_up_plugins() {
-  wp --allow-root plugin delete $(wp --allow-root plugin list --status=inactive --field=name)
-}
-
-wp_clean_up_themes() {
-  wp --allow-root theme delete $(wp --allow-root theme list --status=inactive --field=name)
+  info 'wp-config.php path: '
+  wp --allow-root config path
 }
 
 wp_install() {
-    wp --allow-root core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_PASSWORD} --admin_email=${WP_EMAIL}
+    wp --allow-root core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_PASSWORD} --admin_email=${WP_EMAIL} --color
+
+    if [ $? -eq 1 ]; then
+      error 'Failed WordPress Install'
+      exit 1
+    fi
+}
+
+wp_install_plugins() {
+  wp --allow-root plugin install ${WP_PLUGINS} --activate --color
+}
+
+wp_clean_up_plugins() {
+  wp --allow-root plugin delete $(wp --allow-root plugin list --status=inactive --field=name) --color
+}
+
+wp_clean_up_themes() {
+  wp --allow-root theme delete $(wp --allow-root theme list --status=inactive --field=name) --color
 }
 
 main() {
+  until nc -z -v -w30 $MYSQL_HOST 3306
+  do
+    info "Waiting for database connection..."
+    info "If mysql container is not running, press Ctrl+C to shutdown this script."
+    sleep 5
+  done
+
+  success 'Database connection established!'
+
   if ! $(wp --allow-root core is-installed); then
     info 'Start WordPress Installation'
+
+    # Check WordPress Core files
     wp_download
 
-    until nc -z -v -w10 $MYSQL_HOST 3306
-    do
-      info "Waiting for database connection..."
-      sleep 5
-    done
-
-    success 'Database established!'
-
     # Check exists wp-config.php
-    wp --allow-root config path --quiet
-    hasConfig=$?
-
-    if [ $hasConfig -ne 0 ]; then
-      info 'Not found wp-config.php'
-      info 'Generating wp-config.php'
-      wp_create_config
-    fi
-
-    info 'wp-config.php path: '
-    wp --allow-root config path
+    wp_create_config
 
     # Install WordPress
     wp_install
@@ -99,7 +106,12 @@ main() {
     # Clean up themes which are inactive
     # Because of security risk that having old theme
     wp_clean_up_themes
+
+    exit 0
   fi
+
+  success 'WordPress has been installed!'
+  exit 0
 }
 
 main
